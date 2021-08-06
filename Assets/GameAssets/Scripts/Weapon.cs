0189e5d0da42a88d2c065339012a1d3d6744cfbc
Zekiah-A <zekiahamoako@outlook.com>
2021-08-06 23:30:02 +0100

    Reloading logic

--- Assets/GameAssets/Scripts/Weapon.cs
+++ Assets/GameAssets/Scripts/Weapon.cs
@@ -3,9 +3,10 @@ using System;
 	//BASE CLASS FOR ALL WEAPONS
 public class Weapon : Spatial
 {
-	[Export] public int MaxAmmo;
-	[Export] public int CurrentAmmo;
-	private float swayThreshold = 10;
+	[Export] public int TotalAmmo = 16;
+	[Export] public int MagazineSize = 8;
+	[Export] public int CurrentAmmo = 8;
+	private float swayThreshold = 4; //How much a mouse must be pushed to trigger.
 	private readonly float swayLeft = -0.5f;
 	private readonly float swayRight = 0.5f;
 	private readonly float swayStrength = 5;
@@ -19,9 +20,11 @@ public class Weapon : Spatial
 	private Spatial player;
 	private Camera playerCamera;
 	private Tween weaponTween;
-	private float mouseRelativeMovement;
+	private Label ammoHud;
+	AnimationPlayer animPlayer;
 
-
+	private float mouseRelativeMovement = 0;
+	private bool CanFire = true;
 
 	public override void _Ready() //better idea, extend for melee
 	{
@@ -41,13 +44,19 @@ public class Weapon : Spatial
 		playerCamera = player.GetNode("SpringArm").GetNode<Camera>("Camera");
 
 		weaponTween = GetNode<Tween>("WeaponTween");
+		ammoHud = GetNode("WeaponHUD").GetNode<Label>("Ammo");
+
+		animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+
+		Reload();
 	}
 
 	public override void _UnhandledInput(InputEvent @event)
 	{
 		if (@event.IsActionPressed("game_fire") && GetParent<InventoryItem>().Enabled)
 			Fire();
-
+		if (@event.IsActionPressed("game_reload") && GetParent<InventoryItem>().Enabled)
+			Reload();
 		if (@event is InputEventMouseMotion eventMouseMotion && Input.GetMouseMode() == Input.MouseMode.Captured)
 			mouseRelativeMovement = -eventMouseMotion.Relative.x;
 	}
@@ -57,14 +66,22 @@ public class Weapon : Spatial
 		SwayWeapon(delta);
 	}
 
-	public void Fire()
+	public virtual void Fire()
 	{
-		AnimationPlayer animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		
 		RayCast weaponRay = GetNode<RayCast>("WeaponCast");
 
-		if (!animPlayer.IsPlaying()) //this kinda limits it to spamming at anim length (0.2 seconds, i could make this the timer length too thru script :thinking:)
+		if (!animPlayer.IsPlaying() && CanFire == true) //this kinda limits it to spamming at anim length (0.2 seconds, i could make this the timer length too thru script :thinking:)
 		{
-			animPlayer.Play("WeaponFire");
+			CurrentAmmo--;
+			if (CurrentAmmo != 0)
+			{
+				animPlayer.Play("WeaponFire");
+				ammoHud.Text = $"{CurrentAmmo}/{MagazineSize} | {TotalAmmo}";
+			}
+			else //reload instead of fire, or reload then fire?
+				Reload();
+
 			muzzleFlash.Visible = true; //timer and then false
 			weaponTimer.Start();
 
@@ -95,9 +112,30 @@ public class Weapon : Spatial
 		} //TODO: Call a "shake" animation in the camera.
 	}
 
-	public void OnFireEnd()
+	public virtual void OnFireEnd()
 	{
 		muzzleFlash.Visible = false;		
+	}
+
+	public virtual void Reload()
+	{
+		animPlayer.Play("WeaponReload");
+
+		int diff = MagazineSize - CurrentAmmo;
+		
+		if (TotalAmmo - diff >= 0)
+		{
+			TotalAmmo -= diff;
+			CurrentAmmo += diff;
+		}
+		else
+		{
+			CurrentAmmo = TotalAmmo;
+			TotalAmmo = 0;
+			if (CurrentAmmo == 0)
+				CanFire = false;
+		}
+		ammoHud.Text = $"{CurrentAmmo}/{MagazineSize} | {TotalAmmo}";
 	}
 
 	public virtual void SwayWeapon(float delta)
