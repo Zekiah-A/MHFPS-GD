813d2e8c9782fee44ca374e38c77df6537f717c5
Zekiah-A <zekiahamoako@outlook.com>
2021-08-23 16:12:13 +0100

    Title / Lobby camera effects

---
+++ Assets/GameAssets/Scripts/CameraEffects.cs
@@ -0,0 +1,56 @@
+using Godot;
+using System;
+
+public class CameraEffects : Camera
+{
+	//private Camera camera;
+	[Export] public bool Random = false;
+	[Export] public bool Inverse = false;
+	[Export] public float Damping = 10f;
+	[Export] public float LerpStrength = 0.1f;
+
+	private float posx = 0, posy = 0;
+
+	private Random random;
+	private Timer cameraTimer;
+
+	public override void _Ready()
+	{
+		if (Random)
+		{
+			random = new Random();
+			try
+			{
+				cameraTimer = GetNode<Timer>("Timer");
+				cameraTimer.Connect("timeout", this, nameof(OnTimerComplete));
+				cameraTimer.Start();
+			}
+			catch (Exception e)
+			{
+				GD.Print($"Random camera movement requires a timer with autostart enabled as a child.\n{e}");
+			}
+		}
+	}
+	public override void _Process(float delta) //input tween
+	{
+		if (!Random)
+		{
+			Vector2 mousePosition = GetViewport().GetMousePosition();
+			posx = (mousePosition.x  - GetViewport().GetVisibleRect().Size.x / 2) / GetViewport().GetVisibleRect().Size.x;
+			posy = (mousePosition.y  - GetViewport().GetVisibleRect().Size.y / 2) / GetViewport().GetVisibleRect().Size.y;
+		}
+
+		/*camera.*/Rotation = new Vector3 (
+			Mathf.Lerp(/*camera.*/Rotation.x, Inverse ? posy / Damping : -posy / Damping, LerpStrength),
+			Mathf.Lerp(/*camera.*/Rotation.y, Inverse ? posx / Damping : -posx / Damping, LerpStrength),
+			/*camera.*/Rotation.z
+		);
+	}
+
+	private void OnTimerComplete()
+	{
+		///<note> Value must be between -0.5 and 0.5 </note>
+		posx = (float) random.Next(-5, 5) / 10.0f;
+		posy = (float) random.Next(-5, 5) / 10.0f;
+	}
+}
