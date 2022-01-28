b020ab0b2942a08827e624858a62528370550653
Zekiah-A <zekiahamoako@outlook.com>
2022-01-28 20:21:29 +0000

    WIP: Detention room modelling & battery depletion

--- Assets/GameAssets/Scripts/Doors.cs
+++ Assets/GameAssets/Scripts/Doors.cs
@@ -4,42 +4,68 @@ using System.Collections.Generic;
 
 public class Doors : Spatial
 {
-    public bool LeftOpened = true;
-    public bool RightOpened = true;
-    public bool VentLit = false;
+	public bool LeftOpened = true;
+	public bool RightOpened = true;
+	public bool VentLit = false;
+	public int BatteryPercentage;
+	
+	public Spatial leftDoor;
+	public Spatial rightDoor;
+	public Timer batteryTimer;
+	public Label batteryLabel;
 
-    public Spatial leftDoor;
-    public Spatial rightDoor;
+	public override void _Ready()
+	{
+		leftDoor = GetNode<Spatial>("LeftDoor");
+		rightDoor = GetNode<Spatial>("RightDoor");
+		batteryTimer = GetNode<Timer>("BatteryTimer");
+		batteryLabel = GetTree().CurrentScene.GetNode("DetentionUI").GetNode("BatteryPanel").GetNode<Label>("Battery");
 
-    public override void _Ready()
-    {
-        leftDoor = GetNode<Spatial>("LeftDoor");
-        rightDoor = GetNode<Spatial>("RightDoor");
-    }
+		BatteryPercentage = 100;
+		batteryTimer.Start();
+	}
 
-    //Temporary code.
-    public void OpenLeft()
-    {
-        leftDoor.Visible = false;
-        LeftOpened = true;
-    }
-    
-    public void CloseLeft()
-    {
-        leftDoor.Visible = true;
-        LeftOpened = false;
-    }
+	public void OpenLeft()
+	{
+		leftDoor.Visible = false;
+		LeftOpened = true;
+	}
+	
+	public void CloseLeft()
+	{
+		leftDoor.Visible = true;
+		LeftOpened = false;
+	}
 
-    public void OpenRight()
-    {
-        rightDoor.Visible = false;
-        RightOpened = true;
+	public void OpenRight()
+	{
+		rightDoor.Visible = false;
+		RightOpened = true;
 
-    }
+	}
 
-    public void CloseRight()
-    {
-        rightDoor.Visible = true;
-        RightOpened = false;
-    }
+	public void CloseRight()
+	{
+		rightDoor.Visible = true;
+		RightOpened = false;
+	}
+
+	public void TorchOnVent()
+	{
+		VentLit = true;
+	}
+
+	public void TorchOffVent()
+	{
+		VentLit = false;
+	}
+	
+	private void OnBatteryTimerTimeout()
+	{
+		//TODO: Make this proper
+		BatteryPercentage--;
+		batteryLabel.Text = $"{BatteryPercentage}%";
+	}
 }
+
+
