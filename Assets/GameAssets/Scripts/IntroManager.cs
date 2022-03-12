dae7f39a927ff9c2ccd1c2b8653f1d515236ec10
Zekiah-A <zekiahamoako@outlook.com>
2022-03-12 19:46:06 +0000

    Iso periskeri model improvements and intro optimisations

--- Assets/GameAssets/Scripts/IntroManager.cs
+++ Assets/GameAssets/Scripts/IntroManager.cs
@@ -1,25 +1,32 @@
 using Godot;
 using System;
+using DiscordRPC.Exceptions;
 
 public class IntroManager : Spatial
 {
 	private Timer creditsTimer;
 	private AnimationPlayer cameraAnimationPlayer;
+	private uint defaultCullMask;
 	
 	public override async void _Ready()
 	{
 		creditsTimer = GetNode<Timer>("CutsceneTimer");
 		cameraAnimationPlayer = GetNode<AnimationPlayer>("Camera/CameraAnimationPlayer");
-
 		var creditsAnimationPlayer = GetNode<AnimationPlayer>("IntroUI/CreditsPanel/AnimationPlayer");
+		
+		//Stop camera from rendering while the intro is playing, make sure to reset the cull mask to original value to restore render.
+		defaultCullMask = GetNode<Camera>("Camera").CullMask;
+		GetNode<Camera>("Camera").CullMask = 0;
 		creditsAnimationPlayer.Play("intro_credits");
-
+		
+		//Wait for the intro GUI cutscene to start, before playing intro.
 		await ToSignal(creditsAnimationPlayer, "animation_finished");
 		PlayIntro();
 	}
 
 	private void PlayIntro()
 	{
+		GetNode<Camera>("Camera").CullMask = defaultCullMask;
 		GetNode<Panel>("IntroUI/CreditsPanel").Visible = false;
 		GetNode<ColorRect>("IntroUI/CreditsBackground").Visible = false;
 
